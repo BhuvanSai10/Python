c4217f80cd53f6cf5ea4fcc1d8dc8a080e013673
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-05-19 22:20:32 +0530

    new file added

---
+++ findLargeNumber.py
@@ -0,0 +1,13 @@
+def findLargerNumber(a,b,c):
+    if a>b:
+        if a>c:
+            return a
+        else:
+            return c
+    else:
+        if b>c:
+            return b
+        else:
+            return c
+
+print(findLargerNumber(3,1,2))
