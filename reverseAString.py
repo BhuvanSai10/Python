6a01ff6114e8ab92de0162b5ae428c553b75ee37
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-05-19 22:57:29 +0530

    new file added

--- reverseAString.py
+++ reverseAString.py
@@ -1,10 +1,11 @@
 def reverse(s):
-    i = len(s) - 1
-    rev=""
-    while(i>=0):
-        rev = rev + s[i]
-        i = i-1
+    # i = len(s) - 1
+    # rev=""
+    # while(i>=0):
+    #     rev = rev + s[i]
+    #     i = i-1
     
-    return rev
+    # return rev
+    return s[::-1]
 
 print(reverse("bhuvansai"))
